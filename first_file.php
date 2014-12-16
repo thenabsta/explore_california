4f951a56abd3862851894a7756038f387ee1d5d1
Kevin Nabity <thenabsta@gmail.com>
2014-12-16 13:13:48 -0600

    Modified title of index.html

--- first_file.php
+++ first_file.php
@@ -1,1 +1,1 @@
-This is a the first file added to my project.  It comes before all other files.  I am adding this change to this branch.
+This is a the first file added to my project.  It comes before all other files.  
