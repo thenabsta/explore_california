1e6b9e831f449b3bd4d9c73764c87cbe9356e215
Kevin Nabity <thenabsta@gmail.com>
2014-12-16 19:45:08 -0600

    Edit first_file.php

--- first_file.php
+++ first_file.php
@@ -1,1 +1,1 @@
-This is a the first file added to my project.  It comes before all other files.  
+Im going to say this is the first file.  This is a the first file added to my project.  It comes before all other files.  
